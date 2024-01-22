5cccac4eac9f493ec1cb494ea119da9cb5bf0703
pinkadotted <m.subesh@gmail.com>
2024-01-23 00:10:12 +0800

    changed border color

--- beep-searchbar/src/components/Card.tsx
+++ beep-searchbar/src/components/Card.tsx
@@ -3,7 +3,7 @@ import SyncSearchBar from "./SyncSearchBar"
 
 const Card = () => {
   return (
-    <div className="flex flex-col border-2 border-green-500 w-1/4 h-1/2">
+    <div className="flex flex-col border-2 border-blue-500 w-1/4 h-1/2">
         <AsyncSearchBar />
         <SyncSearchBar label='Country search' description='Search for a country' />
     </div>
