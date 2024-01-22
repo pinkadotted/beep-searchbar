e16fe954f30e601c4fda256e23aa260c3d9e62c9
pinkadotted <m.subesh@gmail.com>
2024-01-22 20:20:50 +0800

    created a Card component

---
+++ beep-searchbar/src/components/Card.tsx
@@ -0,0 +1,13 @@
+import AsyncSearchBar from "./AsyncSearchBar"
+import SyncSearchBar from "./SyncSearchBar"
+
+const Card = () => {
+  return (
+    <div className="flex flex-col border-2 border-green-500 w-1/4 h-1/2">
+        <AsyncSearchBar />
+        <SyncSearchBar label='Country search' description='Search for a country' />
+    </div>
+  )
+}
+
+export default Card
