dc7be9c29c7ff1d4ab743a49d10568a92ffad5a7
pinkadotted <m.subesh@gmail.com>
2024-01-23 23:05:32 +0800

    added isLoading logic to AsyncSearchBar

--- beep-searchbar/src/components/AsyncSearchBar.tsx
+++ beep-searchbar/src/components/AsyncSearchBar.tsx
@@ -17,6 +17,8 @@ const AsyncSearchBar: React.FC<AsyncSearchBarProps> = ({
   const [selectedItemsList, setSelectedItemsList] = useState([]);
   const [isFocused, setIsFocused] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   const countries: any = Data;
 
   const inputRef = useRef<HTMLInputElement>(null);
@@ -26,15 +28,21 @@ const AsyncSearchBar: React.FC<AsyncSearchBarProps> = ({
   const debounce = (func: any, wait: number) => {
     let timeout: any;
     return function executedFunction(...args: any) {
+      isLoadingHandler(true);
       const later = () => {
         clearTimeout(timeout);
         func(...args);
+        isLoadingHandler(false);
       };
       clearTimeout(timeout);
       timeout = setTimeout(later, wait);
     };
-  }
+  };
 
+  const isLoadingHandler = (isLoading: boolean) => {
+    console.log("isLoadingHandler: ", isLoading);
+    setIsLoading(isLoading);
+  };
 
   const handleKeyDown = (e: React.KeyboardEvent) => {
     if (e.key === "ArrowDown" || e.key === "ArrowUp" || e.key === "Escape") {
@@ -47,7 +55,7 @@ const AsyncSearchBar: React.FC<AsyncSearchBarProps> = ({
   const isRefFocusedHandler = (ref: React.RefObject<any>) => {
     // setIsInputFocused(isInputFocused);
     ref.current?.focus();
-  }
+  };
 
   const selectedItemsHandler = (selectedItems: any) => {
     setSelectedItemsList(selectedItems);
@@ -56,13 +64,13 @@ const AsyncSearchBar: React.FC<AsyncSearchBarProps> = ({
   const isFocusedHandler = (isFocused: boolean) => {
     setIsFocused(isFocused);
     console.log(isFocused);
-  }
+  };
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log("handleSearch: ", e.target.value);
     setShowDropdown(false);
     setSearchTerm(e.target.value);
-    
+
     debouncedFilterCountries(countries);
   };
 
@@ -82,16 +90,19 @@ const AsyncSearchBar: React.FC<AsyncSearchBarProps> = ({
         country.name.toLowerCase().includes(searchTerm.toLowerCase())
       );
     });
-  }
+  };
 
-  const debouncedFilterCountries = useCallback(debounce(filterCountries, 1000), [])
-
+  const debouncedFilterCountries = useCallback(
+    debounce(filterCountries, 1000),
+    []
+  );
 
   return (
-    <div className="flex flex-col"
-    onBlur={() => setIsFocused(false)}
-    onFocus={() => setIsFocused(true)}
-    onKeyDown={handleKeyDown}
+    <div
+      className="flex flex-col h-20 relative items-center"
+      onBlur={() => setIsFocused(false)}
+      onFocus={() => setIsFocused(true)}
+      onKeyDown={handleKeyDown}
     >
       {/* input label */}
       <label
@@ -100,36 +111,44 @@ const AsyncSearchBar: React.FC<AsyncSearchBarProps> = ({
       >
         {label}
       </label>
-      <input
-        className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none "
-        type="text"
-        placeholder="Search..."
-        value={searchTerm}
-        onChange={handleSearch}
-        ref={inputRef}
-        // onKeyDown={handleKeyDown}
-        // onBlur={() => setIsFocused(false)}
-      />
+      <div className="relative">
+        <input
+          className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleSearch}
+          ref={inputRef}
+        />
+        {isLoading && (
+          <div className="absolute inset-y-0 right-0 flex items-center mr-3 pointer-events-none">
+            <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-blue-500 border-opacity-50"></div>
+          </div>
+        )}
+      </div>
+
       {/* input description */}
       <p className="text-gray-500 text-xs italic">{description}</p>
 
       {/* input results */}
       {
-      // searchTerm !== ""
-      (showDropdown && isFocused)
-       && 
-       <Dropdown 
-       options={filteredCountries} 
-       selectedItemsHandler={selectedItemsHandler} 
-       selectedItems={selectedItemsList} 
-       isFocusedHandler={isFocusedHandler} 
-       isRefFocusedHandler={isRefFocusedHandler}
-       dropdownRef={dropdownRef}
-       inputRef={inputRef}
-       />}
+        // searchTerm !== ""
+        showDropdown && isFocused && (
+          <div className="absolute mt-20 px-2.5 w-full z-10">
+            <Dropdown
+              options={filteredCountries}
+              selectedItemsHandler={selectedItemsHandler}
+              selectedItems={selectedItemsList}
+              isFocusedHandler={isFocusedHandler}
+              isRefFocusedHandler={isRefFocusedHandler}
+              dropdownRef={dropdownRef}
+              inputRef={inputRef}
+            />
+          </div>
+        )
+      }
     </div>
   );
 };
 
-
-export default AsyncSearchBar
+export default AsyncSearchBar;
